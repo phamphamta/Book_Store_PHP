dcce71eab3c89fa47d9e07e411eea91200edf47f
Phạm Tuấn Anh <pphamta16@gmail.com>
2025-06-23 21:35:24 +0700

    edit next

--- config.php
+++ config.php
@@ -1,10 +1,14 @@
 <?php
 $url = parse_url(getenv("MYSQL_PUBLIC_URL"));
 
+if (!$url) {
+    die("Không tìm thấy biến môi trường MYSQL_PUBLIC_URL");
+}
+
 $host = $url["host"];
 $user = $url["user"];
 $pass = $url["pass"];
-$db   = ltrim($url["path"], "/");
+$db   = ltrim($url["path"], '/');
 $port = $url["port"];
 
 $conn = mysqli_connect($host, $user, $pass, $db, $port);
