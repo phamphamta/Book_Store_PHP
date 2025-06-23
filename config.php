0c33a522f315dc11dc0a914ea45a137253962707
Phạm Tuấn Anh <pphamta16@gmail.com>
2025-06-23 11:01:04 +0700

    edit config

--- config.php
+++ config.php
@@ -1,11 +1,11 @@
 <?php
-$conn = mysqli_connect(
-    getenv('MYSQLHOST'),
-    getenv('MYSQLUSER'),
-    getenv('MYSQLPASSWORD'),
-    getenv('MYSQLDATABASE'),
-    getenv('MYSQLPORT')
-);
+$host = getenv('MYSQLHOST');
+$user = getenv('MYSQLUSER');
+$password = getenv('MYSQLPASSWORD');
+$database = getenv('MYSQLDATABASE');
+$port = getenv('MYSQLPORT') ?: 3306;
+
+$conn = mysqli_connect($host, $user, $password, $database, $port);
 
 if (!$conn) {
     die("Kết nối thất bại: " . mysqli_connect_error());
